c5278621a40423b65e5e42a59a44f8dfe975ba81
Weyner Ccuiro <wey.090300@gmail.com>
2021-10-19 01:43:01 -0500

    matexample sin animacion terminado

--- animation.py
+++ animation.py
@@ -1,26 +1,37 @@
-"""
-A simple example of an animated plot
-"""
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
-fig, ax = plt.subplots()
-
-x = np.arange(0, 2*np.pi, 0.01)
-line, = ax.plot(x, np.sin(x))
+import numpy as np
 
 
-def animate(i):
-    line.set_ydata(np.sin(x + i/10.0))  # update the data
-    return line,
+class PauseAnimation:
+    def __init__(self):
+        fig, ax = plt.subplots()
+        ax.set_title('Click to pause/resume the animation')
+        x = np.linspace(-0.1, 0.1, 1000)
+
+        # Start with a normal distribution
+        self.n0 = (1.0 / ((4 * np.pi * 2e-4 * 0.1) ** 0.5)5
+                   * np.exp(-x ** 2 / (4 * 2e-4 * 0.1)))
+        self.p, = ax.plot(x, self.n0)
+
+        self.animation = animation.FuncAnimation(
+            fig, self.update, frames=200, interval=50, blit=True)
+        self.paused = False
+
+        fig.canvas.mpl_connect('button_press_event', self.toggle_pause)
+
+    def toggle_pause(self, *args, **kwargs):
+        if self.paused:
+            self.animation.resume()
+        else:
+            self.animation.pause()
+        self.paused = not self.paused
+
+    def update(self, i):
+        self.n0 += i / 100 % 5
+        self.p.set_ydata(self.n0 % 20)
+        return (self.p,)
 
 
-# Init only required for blitting to give a clean slate.
-def init():
-    line.set_ydata(np.ma.array(x, mask=True))
-    return line,
-
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
-                              interval=25, blit=True)
+pa = PauseAnimation()
 plt.show()
--- matexample.py
+++ matexample.py
@@ -6,31 +6,50 @@ import numpy as np
 import math as ma
 
 # definiendo la funcion de la posicion
-
-
 def position_plot(t, initial_position, initial_velocity, frecuency):
     return initial_position*np.cos(frecuency*t)+initial_velocity/frecuency*np.sin(frecuency*t)
 
 # definieno la funcion de la velocidad
-
-
 def velocity_plot(t, initial_position, initial_velocity, frecuency):
     return -initial_position*frecuency*np.sin(frecuency*t)+initial_velocity*np.cos(frecuency*t)
 
+#funcion recta de soporte del resorte
+def recta(largo ,alto):
+    return largo*alto
+
+#funcion que retorna un resorte dinamico
+def spring(xo,yo,xf,yf,width,points):
+    cicle=(-1,0,1)
+    j=0
+    x=[xo]
+    y=[yo]
+    spr=[(xo,yo)]
+    for i in range(points):
+        if j>=3:
+            j=0
+        spr.append((int(xo+width*cicle[j]),int(yo+(i+1)/points*(yf-yo))))
+        x.append(xo+width*cicle[j])
+        y.append(yo+(i+1)/points*(yf-yo))
+        j+=1
+    return x, y
+
 # formateo de los ejes para que todo se vea mas bonito
-
-
 def format_axes(fig):
     for i, ax in enumerate(fig.axes):
         #ax.text(0.5, 0.5, "ax%d" % (i+1), va="center", ha="center")
-        ax.tick_params(labelbottom=False, labelleft=True)
+        graph.tick_params(labelbottom=False, labelleft=False)
+        graph.set_xlim([0, 10])
+        graph.set_ylim([0, 14])
         if ax==pos_ax or ax==vel_ax:
+            ax.tick_params(labelbottom=False, labelleft=True)
             #para ponerle la mallita
             ax.grid(True)
             if ax==vel_ax:
+                # ax.suptitle("Velocidad")
                 ax.set_xlim([0, 10])
                 ax.set_ylim([-23, 23])
             else:#para poner los limites
+                # ax.suptitle("Posicion")
                 ax.set_xlim([0, 10])
                 ax.set_ylim([-7, 7])
 
@@ -39,18 +58,19 @@ def format_axes(fig):
 t = np.linspace(0, 10, 200)
 
 # valores inciales de las constantes
-initial_position = 0
-initial_velocity = 0
+initial_position = 2
+initial_velocity = 1
+initial_frequency = 6
 
 # Creando la figura
-fig = plt.figure(constrained_layout=True)
+fig = plt.figure(constrained_layout=False)
 
 '''
 poniendo cada plot en un Grid o malla, y es de la siguiente manera
 gs[pos_x:posy,filas:columnas] o algo asi, sale probando xd
 '''
 gs = GridSpec(2, 2, figure=fig)
-ax1 = fig.add_subplot(gs[0:2, 0:1])
+graph = fig.add_subplot(gs[0:2, 0:1])
 pos_ax = fig.add_subplot(gs[0, 1:])
 vel_ax = fig.add_subplot(gs[1, 1:])
 
@@ -59,7 +79,7 @@ vel_ax = fig.add_subplot(gs[1, 1:])
 # k = float(input("Ingrese el valor de la constante de elasticidad: "))
 # m = float(input("Ingrese el valor de la masa: "))
 #o talvez es mejor solo manejar la constante w
-w=float(input("Ingrese el valor de la frecuencia: "))
+#w=float(input("Ingrese el valor de la frecuencia: "))
 
 
 # yo=float(input("Ingrese la posición inicial: "))
@@ -67,19 +87,36 @@ w=float(input("Ingrese el valor de la frecuencia: "))
 
 #w = (k/m)**(0.5)
 
+'''
+Grafico del resorte con la masa
+'''
+#grafico de recta
+ancho_de_soporte=np.linspace(2,8,3)
+graph.plot(ancho_de_soporte, recta(np.ones(len(ancho_de_soporte)),12), color='black')
 
-pos_ax.plot(t, position_plot(t, initial_position, initial_velocity, w))
-vel_ax.plot(t, velocity_plot(t, initial_position, initial_velocity, w))
+#grafico de resorte
+x, y = spring(5,12,5,7,0.5,26)
+graph.plot(x,y, color='black')
+
+#grafico de la masa
+masa = plt.Circle((5,7),0.3, color='black')
+graph.add_patch(masa)
+
+'''
+grafico de la posicion vs la velocidad
+'''
+pos_ax.plot(t, position_plot(t, initial_position, initial_velocity, initial_frequency))
+vel_ax.plot(t, velocity_plot(t, initial_position, initial_velocity, initial_frequency))
 
 # adjust the main plot to make room for the sliders
-#plt.subplots_adjust(left=0.25, bottom=0.25)
+plt.subplots_adjust(left=0.01, right=0.99, bottom=0.03, top=0.94, wspace=0.4)
 axcolor = 'lightgoldenrodyellow'
 
 #slider posicion
-axpos = plt.axes([0.25, 0.1, 0.03, 0.65], facecolor=axcolor)
+axpos = plt.axes([0.54, 0.05, 0.012, 0.85], facecolor=axcolor)
 pos_slider = Slider(
     ax=axpos,
-    label='Position [cm]',
+    label='Pos [cm]',
     valmin=-5,
     valmax=5,
     valinit=initial_position,
@@ -87,13 +124,24 @@ pos_slider = Slider(
 )
 
 #slider velocidad
-axvel = plt.axes([0.1, 0.25, 0.0225, 0.63], facecolor=axcolor)
+axvel = plt.axes([0.49, 0.05, 0.012, 0.85], facecolor=axcolor)
 vel_slider = Slider(
     ax=axvel,
-    label="Velocity [cm/s]",
+    label="Vel [cm/s]",
     valmin=-5,
     valmax=5,
     valinit=initial_velocity,
+    orientation="vertical"
+)
+
+#slider frecuencia
+axfeq = plt.axes([0.44, 0.05, 0.012, 0.85], facecolor=axcolor)
+feq_slider = Slider(
+    ax=axfeq,
+    label="Freq [$s^{-1}$]",
+    valmin=1,
+    valmax=10,
+    valinit=initial_frequency,
     orientation="vertical"
 )
 
@@ -101,8 +149,8 @@ vel_slider = Slider(
 def update_graphs(val):
     pos_ax.clear()
     vel_ax.clear()
-    pos_ax.plot(t, position_plot(t, pos_slider.val, vel_slider.val, w))
-    vel_ax.plot(t, velocity_plot(t, pos_slider.val, vel_slider.val, w))
+    pos_ax.plot(t, position_plot(t, pos_slider.val, vel_slider.val, feq_slider.val))
+    vel_ax.plot(t, velocity_plot(t, pos_slider.val, vel_slider.val, feq_slider.val))
     format_axes(fig)
     fig.canvas.draw_idle()
 
@@ -110,8 +158,9 @@ def update_graphs(val):
 #registro de los cambios en los sliders
 pos_slider.on_changed(update_graphs)
 vel_slider.on_changed(update_graphs)
+feq_slider.on_changed(update_graphs)
 
-fig.suptitle("GridSpec")
+fig.suptitle("Oscilador Armónico Simple")
 format_axes(fig)
 
 plt.show()
